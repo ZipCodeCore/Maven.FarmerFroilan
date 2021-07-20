8e7d56b1104ca113242b1fb648ad8919b404ddf1
Nathan <nathan@zipcodes-MacBook-Pro-9.local>
2021-07-20 19:53:33 -0400

    edible interface

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/Animal.java
@@ -0,0 +1,12 @@
+package com.zipcodewilmington.froilansfarm;
+
+public class Animal implements Eater, NoiseMaker {
+
+    public void eat(Object o) {
+
+    }
+
+    public String makeNoise() {
+        return null;
+    }
+}

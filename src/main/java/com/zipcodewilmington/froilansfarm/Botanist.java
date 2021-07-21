73b86bfcb52abd9828f20f74df36cf6cddc52bf8
jgiroso <jrgiroso@gmail.com>
2021-07-20 23:01:28 -0400

    person tests

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/Botanist.java
@@ -0,0 +1,6 @@
+package com.zipcodewilmington.froilansfarm;
+
+public interface Botanist {
+
+    void plant(Crop crop, CropRow cropRow);
+}
---
+++ src/test/java/com/zipcodewilmington/froilansfarm/PersonTest.java
@@ -0,0 +1,29 @@
+package com.zipcodewilmington.froilansfarm;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PersonTest {
+
+    @Test
+    public void testConstructor() {
+        //given
+        String expected = "Sophia";
+        Person person = new Person(expected);
+        //when
+        String actual = person.getName();
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetName() {
+        //given
+        String expected = "Tyler";
+        //when
+        Person person = new Person(expected);
+        String actual = person.getName();
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+}

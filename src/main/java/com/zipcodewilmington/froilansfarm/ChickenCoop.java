2bda368111f5971e0d1cd62f425dd5188dba6c66
jgiroso <jrgiroso@gmail.com>
2021-07-20 20:00:27 -0400

    added methods to shelter classes

--- src/main/java/com/zipcodewilmington/froilansfarm/ChickenCoop.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/ChickenCoop.java
@@ -1,5 +1,22 @@
 package com.zipcodewilmington.froilansfarm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChickenCoop implements Shelter<Chicken> {
 
+    List<Chicken> chickenList = new ArrayList<Chicken>();
+    Chicken chicken;
+
+    public void add(Chicken thingToBeStored) {
+        chickenList.add(thingToBeStored);
+    }
+
+    public Chicken getTypeByName(String name) {
+        return chicken.getName();
+    }
+
+    public void remove(Chicken thingToBeStored) {
+        chickenList.remove(thingToBeStored);
+    }
 }

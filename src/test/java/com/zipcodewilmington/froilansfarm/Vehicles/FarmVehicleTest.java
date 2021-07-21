95609db63cf1f3a782ff3176a9ce8632ffbdf2cc
charnaexw <charnaexw@gmail.com>
2021-07-21 16:08:27 -0400

    BareBones Classes and Tests

---
+++ src/test/java/com/zipcodewilmington/froilansfarm/Vehicles/FarmVehicleTest.java
@@ -0,0 +1,16 @@
+package com.zipcodewilmington.froilansfarm.Vehicles;
+
+import org.junit.Test;
+
+public class FarmVehicleTest {
+
+    @Test
+    public class constructorTest(){
+        //given
+
+        //when
+
+        //then
+    }
+
+}
---
+++ src/test/java/com/zipcodewilmington/froilansfarm/Vehicles/TractorTest.java
@@ -0,0 +1,14 @@
+package com.zipcodewilmington.froilansfarm.Vehicles;
+
+import org.junit.Test;
+
+public class TractorTest {
+
+    @Test
+    public class constructorTest(){
+        //given
+        //when
+        //then
+    }
+
+}

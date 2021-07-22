8640b743dcf37491e56b1bcf15a7e9659cbf0ef9
Jeremy <jeremysabina@gmail.com>
2021-07-22 19:03:42 -0400

    Simulation stuff?

--- src/main/java/com/zipcodewilmington/froilansfarm/Simulation.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Simulation.java
@@ -3,7 +3,9 @@ package com.zipcodewilmington.froilansfarm;
 
 import com.zipcodewilmington.froilansfarm.Food.CropRow;
 import com.zipcodewilmington.froilansfarm.Mammal.Chicken;
+import com.zipcodewilmington.froilansfarm.Mammal.Farmer;
 import com.zipcodewilmington.froilansfarm.Mammal.Horse;
+import com.zipcodewilmington.froilansfarm.Mammal.Person;
 import com.zipcodewilmington.froilansfarm.Structure.ChickenCoop;
 import com.zipcodewilmington.froilansfarm.Vehicles.Aircraft;
 import com.zipcodewilmington.froilansfarm.Vehicles.FarmVehicle;
@@ -17,12 +19,43 @@ public class Simulation {
     public static void run() {
 
         Farm farm = new Farm();
+
+        Person froilan = new Farmer("Froilan");
+        Person froilanda = new Person("Froilanda");
+
         Field field = new Field();
         CropRow cropRow = new CropRow();
         ChickenCoop chickenCoop = new ChickenCoop();
-        Chicken chicken = new Chicken();
-        Horse horse = new Horse();
-        FarmVehicle tractor = new Tractor();
+
+        Chicken chicken1 = new Chicken();
+        Chicken chicken2 = new Chicken();
+        Chicken chicken3 = new Chicken();
+        Chicken chicken4 = new Chicken();
+        Chicken chicken5 = new Chicken();
+        Chicken chicken6 = new Chicken();
+        Chicken chicken7 = new Chicken();
+        Chicken chicken8 = new Chicken();
+        Chicken chicken9 = new Chicken();
+        Chicken chicken10 = new Chicken();
+        Chicken chicken11 = new Chicken();
+        Chicken chicken12 = new Chicken();
+        Chicken chicken13 = new Chicken();
+        Chicken chicken14 = new Chicken();
+        Chicken chicken15 = new Chicken();
+
+        Horse horse1 = new Horse();
+        Horse horse2 = new Horse();
+        Horse horse3 = new Horse();
+        Horse horse4 = new Horse();
+        Horse horse5 = new Horse();
+        Horse horse6 = new Horse();
+        Horse horse7 = new Horse();
+        Horse horse8 = new Horse();
+        Horse horse9 = new Horse();
+        Horse horse10 = new Horse();
+
+        FarmVehicle tractor1 = new Tractor();
+        FarmVehicle tractor2 = new Tractor();
         Aircraft cropDuster = new Aircraft();
 
 

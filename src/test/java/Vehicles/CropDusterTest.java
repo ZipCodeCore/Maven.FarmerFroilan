b418771b1fb42ac2b94e409fd6ec7a3889d364a3
Zach <zach@zipcodes-MacBook-Pro-6.local>
2021-07-22 13:01:46 -0400

    Added tests for Tractor

--- src/test/java/Vehicles/CropDusterTest.java
+++ src/test/java/Vehicles/CropDusterTest.java
@@ -1,15 +1,27 @@
 package Vehicles;
 
 import com.zipcodewilmington.froilansfarm.Farm;
-import com.zipcodewilmington.froilansfarm.Vehicles.Aircraft;
-import com.zipcodewilmington.froilansfarm.Vehicles.CropDuster;
-import com.zipcodewilmington.froilansfarm.Vehicles.Vehicle;
+import com.zipcodewilmington.froilansfarm.Vehicles.*;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CropDusterTest {
 
     @Test
-    public void constructor(){
-        
+    public void constructorTest1(){
+        Tractor tractor = new Tractor();
+
+        Boolean actual = tractor instanceof Vehicle;
+
+        Assert.assertTrue(actual);
+    }
+    
+    @Test
+    public void constructorTest2(){
+        Tractor tractor = new Tractor();
+
+        Boolean actual = tractor instanceof FarmVehicle;
+
+        Assert.assertTrue(actual);
     }
 }

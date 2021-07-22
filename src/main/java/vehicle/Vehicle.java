58e596329b3bc020645b58577a4688d61cce9266
Raymond <raymond@zipcoder.local>
2021-07-22 16:43:13 -0400

    vehicles

--- src/main/java/vehicle/Vehicle.java
+++ src/main/java/vehicle/Vehicle.java
@@ -1,4 +1,27 @@
 package vehicle;
 
-public class Vehicle {
+import Interface.NoiseMaker;
+import Interface.Rideable;
+import Interface.Rider;
+
+import java.util.List;
+
+public class Vehicle<SomeTypeOfRider extends Rider> implements NoiseMaker, Rideable {
+    List<SomeTypeOfRider> riders;
+
+    public void addRider(SomeTypeOfRider rider){
+        riders.add(rider);
+    }
+
+    public void removeRider(SomeTypeOfRider rider){
+        riders.remove(rider);
+    }
+
+    public void getNumberOfPassengers(){
+        riders.size();
+    }
+    @Override
+    public String makeNoise() {
+        return "BEEP BEEP!";
+    }
 }

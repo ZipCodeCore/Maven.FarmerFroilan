3264514d802582e583aa8e51df3d354421eafaf5
Zach <zach@zipcodes-MacBook-Pro-6.local>
2021-07-22 17:04:18 -0400

    Further testing on vehicles

--- src/test/java/Vehicles/CropDusterTest.java
+++ src/test/java/Vehicles/CropDusterTest.java
@@ -1,6 +1,8 @@
 package Vehicles;
 
 import com.zipcodewilmington.froilansfarm.Farm;
+import com.zipcodewilmington.froilansfarm.Food.Crop;
+import com.zipcodewilmington.froilansfarm.Food.CropRow;
 import com.zipcodewilmington.froilansfarm.Vehicles.*;
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,19 +11,50 @@ public class CropDusterTest {
 
     @Test
     public void constructorTest1(){
-        Tractor tractor = new Tractor();
+        CropDuster duster = new CropDuster();
 
-        Boolean actual = tractor instanceof Vehicle;
+        Boolean actual = duster instanceof Vehicle;
 
         Assert.assertTrue(actual);
     }
-    
+
     @Test
     public void constructorTest2(){
-        Tractor tractor = new Tractor();
+        CropDuster duster = new CropDuster();
 
-        Boolean actual = tractor instanceof FarmVehicle;
+        Boolean actual = duster instanceof FarmVehicle;
 
         Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void constructorTest3(){
+        CropDuster duster = new CropDuster();
+
+        Boolean actual = duster instanceof Aircraft;
+
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void fertilizeTest(){
+        CropDuster duster = new CropDuster();
+
+        CropRow row = new CropRow();
+        duster.fertilizeCrop(row);
+
+        Boolean actual = row.checkFertilized();
+
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void shootTest(){
+        Aircraft duster = new CropDuster();
+
+        String actual = duster.shoot();
+        String expected = "BULLETS";
+
+        Assert.assertEquals(expected, actual);
     }
 }

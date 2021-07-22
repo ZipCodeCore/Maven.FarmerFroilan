4c6604beb657ccce3404411a831ab134785bb51b
Jeremy <jeremysabina@gmail.com>
2021-07-22 17:47:04 -0400

    Started Simulation

--- src/main/java/com/zipcodewilmington/froilansfarm/Simulation.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Simulation.java
@@ -1,6 +1,13 @@
 package com.zipcodewilmington.froilansfarm;
 
+
 import com.zipcodewilmington.froilansfarm.Food.CropRow;
+import com.zipcodewilmington.froilansfarm.Mammal.Chicken;
+import com.zipcodewilmington.froilansfarm.Mammal.Horse;
+import com.zipcodewilmington.froilansfarm.Structure.ChickenCoop;
+import com.zipcodewilmington.froilansfarm.Vehicles.Aircraft;
+import com.zipcodewilmington.froilansfarm.Vehicles.FarmVehicle;
+import com.zipcodewilmington.froilansfarm.Vehicles.Tractor;
 
 public class Simulation {
     public static void main(String[] args) {
@@ -8,9 +15,16 @@ public class Simulation {
     }
 
     public static void run() {
+
+        Farm farm = new Farm();
         Field field = new Field();
         CropRow cropRow = new CropRow();
+        ChickenCoop chickenCoop = new ChickenCoop();
+        Chicken chicken = new Chicken();
+        Horse horse = new Horse();
+        FarmVehicle tractor = new Tractor();
+        Aircraft cropDuster = new Aircraft();
+
 
     }
-
 }

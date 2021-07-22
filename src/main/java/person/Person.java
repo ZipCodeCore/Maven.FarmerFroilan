1ca65267a9d381f3e88d0c62b63a3b312fb940f0
johnS93 <john.song8293@gmail.com>
2021-07-22 13:36:41 -0400

    added methods to person class

--- src/main/java/person/Person.java
+++ src/main/java/person/Person.java
@@ -1,4 +1,26 @@
 package person;
 
-public class Person {
+import Interface.*;
+
+public abstract class Person implements NoiseMaker, Rider, Eater<Edible> {
+    String name;
+
+    public void Person(String name){
+        this.name = name;
+
+    }
+    public String getName(){
+        return name;
+    }
+
+    public void mount(Rideable rideable){
+
+    }
+    public void disMount(){
+
+    }
+    public void  makeNoise(){
+
+    }
+
 }

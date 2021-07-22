55693eda8193f7fb2daf62087b1dea1a3323c751
charnaexw <charnaexw@gmail.com>
2021-07-22 15:06:55 -0400

    Woop

--- src/main/java/com/zipcodewilmington/froilansfarm/Vehicle/Tractor.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Vehicle/Tractor.java
@@ -1,4 +1,37 @@
 package com.zipcodewilmington.froilansfarm.Vehicle;
 
 public class Tractor extends FarmVehicle {
+    boolean toBeHarvested;
+    int numOfFertCrop;
+    int numOfHarvCrop;
+
+    public Tractor(boolean toBeHarvested, int numOfFertCrop, int numOfHarvCrop) {
+        this.toBeHarvested = toBeHarvested;
+        this.numOfFertCrop = numOfFertCrop;
+        this.numOfHarvCrop = numOfHarvCrop;
+    }
+
+    public boolean isToBeHarvested() {
+        return toBeHarvested;
+    }
+
+    public void setToBeHarvested(boolean toBeHarvested) {
+        this.toBeHarvested = toBeHarvested;
+    }
+
+    public void setNumOfFertCrop(int numOfFertCrop) {
+        this.numOfFertCrop = numOfFertCrop;
+    }
+
+    public void setNumOfHarvCrop(int numOfHarvCrop) {
+        this.numOfHarvCrop = numOfHarvCrop;
+    }
+
+    public int getNumOfHarvCrop() {
+        return 0;
+    }
+
+    public int getNumOfFertCrop() {
+        return 0;
+    }
 }
--- src/test/java/com/zipcodewilmington/froilansfarm/Vehicles/TractorTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/Vehicles/TractorTest.java
@@ -1,5 +1,8 @@
 package com.zipcodewilmington.froilansfarm.Vehicles;
 
+import com.zipcodewilmington.froilansfarm.Vehicle.CropDuster;
+import com.zipcodewilmington.froilansfarm.Vehicle.Tractor;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TractorTest {
@@ -7,8 +10,64 @@ public class TractorTest {
     @Test
     public void constructorTest(){
         //given
+        int numOfHarvCrops=93;
+        int numOfFertCrops=32;
+        boolean toBeHarvested=true;
         //when
+        Tractor crop = new Tractor(false, numOfFertCrops, numOfHarvCrops);
+
         //then
+        Assert.assertNotEquals(numOfHarvCrops, crop.getNumOfHarvCrop());
+        Assert.assertNotEquals(numOfFertCrops, crop.getNumOfFertCrop());
+        Assert.assertNotEquals(toBeHarvested, false);
+    }
+    @Test
+    public void nullaryConstructorTest(){
+        //given
+        boolean expectedHarvested=true;
+        int expectedNumOfHarvCrop=895894;
+        int expectedNumOfFertCrop=4863826;
+
+        //when
+        Tractor crop = new Tractor(false, 0,0);
+        //then
+        Assert.assertNotEquals(expectedNumOfHarvCrop, crop.getNumOfHarvCrop());
+        Assert.assertNotEquals(expectedNumOfFertCrop, crop.getNumOfFertCrop());
+        Assert.assertNotEquals(expectedHarvested, crop.isToBeHarvested());
+
+    }
+    @Test
+    public void getCropsTest1(){
+        int expected= 15;
+        CropDuster crop = new CropDuster(false, 0, 0);
+        crop.setNumOfCropRows(expected);
+        //when
+        int actual = crop.getNumOfCropRows();
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void needsToBeFertilizedTest(){
+        //given
+        int cropNum=20;
+        int fertCrop=4;
+        boolean expected=true;
+        //when
+        CropDuster crop =new CropDuster(false, cropNum, fertCrop);
+        boolean actual= crop.needsToBeFertilized(cropNum, fertCrop);
+        //then
+        Assert.assertEquals(expected, actual );
+    }
+
+    @Test
+    public void getFertCropsTest1(){
+        int expectedFertCrop = 5;
+        CropDuster crop = new CropDuster(false,0, 0);
+        crop.setNumOfFertCrop(expectedFertCrop);
+
+        int actual = crop.getNumOfFertCrop();
+
+        Assert.assertEquals(expectedFertCrop, actual);
     }
 
 }

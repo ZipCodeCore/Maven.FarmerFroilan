6d30531ed4fd735ca3da05195e8e7f72f3a88010
Nathan <nathan@zipcodes-MacBook-Pro-9.local>
2021-07-22 20:19:53 -0400

    changed croprow

--- src/main/java/com/zipcodewilmington/froilansfarm/Food/CropRow.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Food/CropRow.java
@@ -8,42 +8,43 @@ import java.util.List;
 public class CropRow {
     private List<Crop> cropList;
 
-    public CropRow(){
+    public CropRow() {
         cropList = new ArrayList<>();
     }
 
-    public void plantCrop(Crop crop){
+    public void plantCrop(Crop crop) {
         this.cropList.add(crop);
     }
 
-    public Integer getNumberOfCrops(){
+    public Integer getNumberOfCrops() {
         return cropList.size();
     }
 
-    public Crop getCropById(Integer id){
+    public Crop getCropById(Integer id) {
         Crop result = null;
-        for(Crop crop: cropList){
+        for (Crop crop : cropList) {
             result = id == crop.getId() ? crop : null;
         }
         return result;
     }
 
 
-
-    public void fertilizeRow(){
+    public void fertilizeRow() {
         cropList.forEach(Crop::fertilize);
     }
 
-    public ArrayList<Edible> harvestRow(){
+    public ArrayList<Edible> harvestRow() {
         ArrayList<Edible> harvest = new ArrayList<>();
-        for (Crop crop: cropList){
+
+        cropList.forEach(crop -> {
             harvest.add(crop.yield());
             crop.setHarvested(true);
-        }
+        });
+
         return harvest;
+
+
     }
-
-
 
 
 }

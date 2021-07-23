926bc43d6c5456dd43c63195ba9884286c12284f
Jeremy <jeremysabina@gmail.com>
2021-07-22 20:16:12 -0400

    Finished Establishing the plot

--- src/main/java/com/zipcodewilmington/froilansfarm/Simulation.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Simulation.java
@@ -8,62 +8,46 @@ import com.zipcodewilmington.froilansfarm.Mammal.Farmer;
 import com.zipcodewilmington.froilansfarm.Mammal.Horse;
 import com.zipcodewilmington.froilansfarm.Mammal.Person;
 import com.zipcodewilmington.froilansfarm.Structure.ChickenCoop;
+import com.zipcodewilmington.froilansfarm.Structure.Stable;
 import com.zipcodewilmington.froilansfarm.Vehicles.Aircraft;
 import com.zipcodewilmington.froilansfarm.Vehicles.FarmVehicle;
 import com.zipcodewilmington.froilansfarm.Vehicles.Tractor;
 
+import java.util.Arrays;
+
 public class Simulation {
     public static void main(String[] args) {
-      run();
+        run();
     }
 
     public static void run() {
 
         Farm farm = new Farm();
-
         Person froilan = new Farmer("Froilan");
-        Person froilanda = new Person("Froilanda");
-
+        Person froilanda = new Pilot();
         Field field = new Field();
-
         EarCorn earCorn = new EarCorn();
-
         CropRow cropRow = new CropRow();
-        ChickenCoop chickenCoop = new ChickenCoop();
-
-
-
-        Chicken chicken1 = new Chicken();
-        Chicken chicken2 = new Chicken();
-        Chicken chicken3 = new Chicken();
-        Chicken chicken4 = new Chicken();
-        Chicken chicken5 = new Chicken();
-        Chicken chicken6 = new Chicken();
-        Chicken chicken7 = new Chicken();
-        Chicken chicken8 = new Chicken();
-        Chicken chicken9 = new Chicken();
-        Chicken chicken10 = new Chicken();
-        Chicken chicken11 = new Chicken();
-        Chicken chicken12 = new Chicken();
-        Chicken chicken13 = new Chicken();
-        Chicken chicken14 = new Chicken();
-        Chicken chicken15 = new Chicken();
-
-        Horse horse1 = new Horse();
-        Horse horse2 = new Horse();
-        Horse horse3 = new Horse();
-        Horse horse4 = new Horse();
-        Horse horse5 = new Horse();
-        Horse horse6 = new Horse();
-        Horse horse7 = new Horse();
-        Horse horse8 = new Horse();
-        Horse horse9 = new Horse();
-        Horse horse10 = new Horse();
-
         FarmVehicle tractor1 = new Tractor();
         FarmVehicle tractor2 = new Tractor();
         Aircraft cropDuster = new Aircraft();
 
 
+
+        Stable stable = new Stable();
+        ChickenCoop chickenCoop = new ChickenCoop();
+        Arrays
+                .asList("Felipe", "Oscar", "Marvey", "Lorraine", "Sally",
+                        "Eggetha Christie", "Dr. Bawk", "Chicklette", "Mz. Cluck",
+                        "Lil peep", "Harry", "Meghen", "Amantha", "Christephanie", "Henrietta")
+                .forEach(name ->chickenCoop.add(new Chicken(name)));
+
+        Arrays
+                .asList("Party Skips Frank Greta Harry Milo Hoovey Yorgle Tom Sarah".split(" "))
+                .forEach(name -> stable.add(new Horse(name)));
+
     }
 }
+
+
+// list.string.map
--- src/main/java/com/zipcodewilmington/froilansfarm/Vehicles/CropDuster.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Vehicles/CropDuster.java
@@ -2,11 +2,20 @@ package com.zipcodewilmington.froilansfarm.Vehicles;
 
 import com.zipcodewilmington.froilansfarm.Farm;
 import com.zipcodewilmington.froilansfarm.Food.CropRow;
+import com.zipcodewilmington.froilansfarm.Pilot;
+import com.zipcodewilmington.froilansfarm.Rider;
 
-public class CropDuster extends Aircraft implements FarmVehicle{
+public class CropDuster<T extends Rider> extends Aircraft implements FarmVehicle {
+   private Pilot pilot;
+
 
    public void fertilizeCrop(CropRow row){
       row.fertilizeRow();
+   }
+
+   @Override
+   public void ride(Rider rider) {
+      this.pilot = pilot;
    }
 
    @Override

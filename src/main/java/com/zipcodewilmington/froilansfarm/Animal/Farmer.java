155f652cd9fc5f3e78350f90cad0ca5301513030
sitarameez <sitarablossom@gmail.com>
2021-07-22 21:50:01 -0400

    farmer

--- src/main/java/com/zipcodewilmington/froilansfarm/Animal/Farmer.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Animal/Farmer.java
@@ -6,46 +6,56 @@ import com.zipcodewilmington.froilansfarm.Rideable;
 import com.zipcodewilmington.froilansfarm.Rider;
 import com.zipcodewilmington.froilansfarm.Vehicle.Botanist;
 import com.zipcodewilmington.froilansfarm.crops.Crop;
+import com.zipcodewilmington.froilansfarm.crops.CropRow;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Farmer extends Person implements Rider, Eater, Botanist {
+    CropRow cropRow = new CropRow();
     Integer numberOfRidesTaken;
     Integer numberOfCropsPlanted;
     Integer numberOfEdiblesEaten;
-    List<Edible> thingsEaten;
-     String name="Froilan";
+    // List<Edible> thingsEaten;
+    String name = "Froilan";
+    Rideable vehicle;
 
 
-    public Farmer( Integer numberOfRidesTaken,Integer numberOfCropsPlanted,Integer numberOfEdiblesEaten
-            ,String name,ArrayList<Edible> thingsEaten){
-        this.numberOfRidesTaken=numberOfRidesTaken;
-        this.numberOfCropsPlanted=numberOfCropsPlanted;
-        this.numberOfEdiblesEaten=numberOfEdiblesEaten;
-        this.name=name;
-        this.thingsEaten = thingsEaten;
+    public Farmer(Integer numberOfRidesTaken, Integer numberOfCropsPlanted, Integer numberOfEdiblesEaten
+            , String name, ArrayList<Edible> thingsEaten) {
+        this.numberOfRidesTaken = numberOfRidesTaken;
+        this.numberOfCropsPlanted = numberOfCropsPlanted;
+        this.numberOfEdiblesEaten = numberOfEdiblesEaten;
+        this.name = name;
+        //this.thingsEaten = thingsEaten;
 
-     }
+    }
 
-     public Farmer(){
-         this.thingsEaten= new ArrayList<Edible>();
-     }
+    public Farmer() {
+    }
 
     public String getName() {
         return name;
     }
 
     public void mount(Rideable object) {
-
+        this.vehicle=object;
+        this.numberOfRidesTaken=0;
+        if (object.hasBeenRiden()) {
+            numberOfRidesTaken++;
+        }
+    }
+    public Integer getNumberOfRidesTaken() {
+        return numberOfRidesTaken;
     }
 
     public void dismount(Rideable object) {
-
+    this.vehicle=null;
     }
 
     public void eat(Edible object) {
-
+        object.isEaten();
+        numberOfEdiblesEaten++;
     }
 
     public boolean hasEaten() {
@@ -53,11 +63,17 @@ public class Farmer extends Person implements Rider, Eater, Botanist {
     }
 
     public void plant(Crop Object) {
-
+        cropRow.addCrop(Object);
+        this.numberOfCropsPlanted = 0;
+        numberOfCropsPlanted++;
     }
 
-    public int setNumberOfPlants() {
-        return 0;
+    public Integer getNumberOfCropsPlanted() {
+        return numberOfCropsPlanted;
+    }
+
+    public void setNumberOfPlants(Integer num) {
+        this.numberOfCropsPlanted = num;
     }
 
     public void setName(String name) {
--- src/test/java/com/zipcodewilmington/froilansfarm/AnimalTest/FarmerTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/AnimalTest/FarmerTest.java
@@ -1,6 +1,10 @@
 package com.zipcodewilmington.froilansfarm.AnimalTest;
 
 import com.zipcodewilmington.froilansfarm.Animal.Farmer;
+import com.zipcodewilmington.froilansfarm.Rideable;
+import com.zipcodewilmington.froilansfarm.crops.CornPlant;
+import com.zipcodewilmington.froilansfarm.crops.Crop;
+import com.zipcodewilmington.froilansfarm.crops.CropRow;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -17,4 +21,36 @@ public class FarmerTest {
         Boolean actual=false;
         Assert.assertFalse(farmer.hasEaten());
     }
+
+    @Test
+    public void plantTest(){
+        Farmer farmer= new Farmer();
+        CropRow cropRow=new CropRow();
+        Integer numberOfCrops=0;
+        Crop crop=new CornPlant();
+Integer expected=1;
+//        cropRow.addCrop(crop);
+farmer.plant(crop);
+        Assert.assertEquals(expected,farmer.getNumberOfCropsPlanted());
+
+    }
+
+    @Test
+    public void mountTest(){
+        Farmer farmer= new Farmer();
+        Integer expected = 1;
+
+        Rideable vehicle = new Rideable() {
+            @Override
+            public boolean hasBeenRiden() {
+                return true;
+            }
+        };
+        farmer.mount(vehicle);
+
+        Assert.assertEquals(expected,farmer.getNumberOfRidesTaken());
+
+    }
+
 }
+

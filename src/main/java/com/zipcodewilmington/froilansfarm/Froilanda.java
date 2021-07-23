8fe133a79c5db26184dde31e01bd6a0d0a0a8b67
Bobbi-Z <85585077+Bobbi-Z@users.noreply.github.com>
2021-07-22 21:49:19 -0400

    getting details hammered out

--- src/main/java/com/zipcodewilmington/froilansfarm/Froilanda.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Froilanda.java
@@ -1,6 +1,10 @@
 package com.zipcodewilmington.froilansfarm;
 
+import com.zipcodewilmington.froilansfarm.animals.person.Person;
 import com.zipcodewilmington.froilansfarm.animals.person.Pilot;
 
-public class Froilanda extends Pilot {
+public class Froilanda extends Person implements Pilot {
+    public void fly() {
+
+    }
 }

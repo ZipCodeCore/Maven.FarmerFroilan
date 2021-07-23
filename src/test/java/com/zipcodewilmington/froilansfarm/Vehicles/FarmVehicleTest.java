144647acbc62c07b62146b22409e0caed3e545b5
NicholasWolak <nick.wolak@hotmail.com>
2021-07-23 11:57:52 -0400

    (feat:nick) implemented more tests on Vehicle package

--- src/test/java/com/zipcodewilmington/froilansfarm/Vehicles/FarmVehicleTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/Vehicles/FarmVehicleTest.java
@@ -4,6 +4,7 @@ import com.zipcodewilmington.froilansfarm.Rideable;
 import com.zipcodewilmington.froilansfarm.Vehicle.CropDuster;
 import com.zipcodewilmington.froilansfarm.Vehicle.FarmVehicle;
 import com.zipcodewilmington.froilansfarm.Vehicle.Vehicle;
+import com.zipcodewilmington.froilansfarm.storage.Farm;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -12,7 +13,7 @@ public class FarmVehicleTest {
     @Test
     public void nullaryConstructorTest(){
         //given
-        String expected= "";
+        String expected = null;
         FarmVehicle farm = new FarmVehicle();
         farm.setLocation(expected);
         //when
@@ -29,5 +30,59 @@ public class FarmVehicleTest {
     public void inheritenceCropDustTest1(){
         FarmVehicle car = new FarmVehicle();
         Assert.assertTrue(car instanceof Rideable);
+    }
+
+    @Test
+    public void getLocationTest () {
+        FarmVehicle thing = new FarmVehicle();
+        String expected = "Over here";
+
+        thing.setLocation("Over here");
+        String actual = thing.getLocation();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void noiseMakerTest () {
+        FarmVehicle thing = new FarmVehicle();
+        String expected = null;
+
+        String actual = thing.noiseMaker();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void hasBeenRiden () {
+        FarmVehicle thing = new FarmVehicle();
+        Boolean expected = false;
+
+        Boolean actual = thing.hasBeenRiden();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void operationOnFarmTest () {
+        FarmVehicle thing = new FarmVehicle();
+        Boolean expected = true;
+        thing.setOnFarm("Is on farm?");
+
+        Boolean actual = thing.operation(thing.getOnFarm());
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test (expected = AssertionError.class)
+    public void operationOffFarmTest () {
+        FarmVehicle thing = new FarmVehicle();
+        Boolean expected = true;
+        thing.setOnFarm("Is on farm?");
+        thing.setLocation("Not on the farm");
+
+        Boolean actual = thing.operation(thing.getLocation());
+
+        Assert.assertEquals(expected, actual);
     }
 }

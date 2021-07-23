3a04befca81fd6c6b1478f779b6321cdbd6956e3
Jeremy <jeremysabina@gmail.com>
2021-07-22 21:16:53 -0400

    hello there

--- src/main/java/com/zipcodewilmington/froilansfarm/Vehicles/Aircraft.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Vehicles/Aircraft.java
@@ -4,18 +4,12 @@ import com.zipcodewilmington.froilansfarm.Rider;
 
 public class Aircraft implements Vehicle{
 
-    public void fly(){}
-
     public String makeNoise() {
         return "Bzzzzeewwwwwwwwwww Rat-tat-tat-tat-tat";
     }
 
     public String shoot(){
         return "BULLETS";
-    }
-
-    public void ride() {
-
     }
 
     @Override

a50dbe15496e7632f8fe87c952206c57256101fc
Luciaw1219 <luciam1219@hotmail.com>
2023-05-16 17:02:20 -0400

    add rideable to horse commented out until rideable is created

--- src/main/java/com/zipcodewilmington/froilansfarm/Horse.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Horse.java
@@ -1,6 +1,6 @@
 package com.zipcodewilmington.froilansfarm;
 
-public class Horse extends Animal {
+public class Horse extends Animal /*implements Rideable */{
 
     public void eat(/*Edible edible*/) {
         System.out.println("munch munch");

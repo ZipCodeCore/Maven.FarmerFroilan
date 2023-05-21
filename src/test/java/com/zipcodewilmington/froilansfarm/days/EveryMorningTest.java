b4dc8dd1b675d171006bff19ded1d7b637cffced
“Darius <dariussprye@gmail.com>
2023-05-21 17:44:05 -0400

    1

--- src/test/java/com/zipcodewilmington/froilansfarm/days/EveryMorningTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/days/EveryMorningTest.java
@@ -3,22 +3,20 @@ package com.zipcodewilmington.froilansfarm.days;
 import com.zipcodewilmington.froilansfarm.Farmer;
 import com.zipcodewilmington.froilansfarm.animals.Horse;
 import com.zipcodewilmington.froilansfarm.buildings.Stable;
+import com.zipcodewilmington.froilansfarm.food.Corn;
+import com.zipcodewilmington.froilansfarm.food.EdibleEgg;
+import com.zipcodewilmington.froilansfarm.food.Tomato;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class EveryMorningTest {
 
-
-    //		* Every morning, `Froilan` and `Froilanda` begin their morning by
-    //			* Riding each `Horse` in each `Stable`.
-    //			* Feeding each `Horse` 3 ear of `Corn`.
-    //			* For breakfast,
-    //				* `Froilan` eats 1 `EarCorn`, 2 `Tomoato`, and 5 `Egg`.
-    //				* `Froilanda` eats 2 `EarCorn`, 1 `Tomoato`, and 2 `Egg`.
-
     @Test
     void everyMorning() {
-        //TODO
-        //GIVEN
+        // TODO
+        // GIVEN
         Farmer farmer = new Farmer("Froilan");
         Farmer farmer2 = new Farmer("Froilanda");
 
@@ -28,14 +26,65 @@ class EveryMorningTest {
         Stable stable1 = new Stable();
         Stable stable2 = new Stable();
 
-        //put horse objects in stable
-        stable1.addHorse(horse1);
-        stable2.addHorse(horse2);
+        Corn corn1 = new Corn();
+        Corn corn2 = new Corn();
+        Corn corn3 = new Corn();
+        Corn corn4 = new Corn();
+        Corn corn5 = new Corn();
+        Corn corn6 = new Corn();
+        Corn corn7 = new Corn();
+        Corn corn8 = new Corn();
+        Corn corn9 = new Corn();
 
-        //add food after plant classes are created
+        Tomato tomato1 = new Tomato();
+        Tomato tomato2 = new Tomato();
+        Tomato tomato3 = new Tomato();
 
-        //
+        EdibleEgg egg1 = new EdibleEgg();
+        EdibleEgg egg2 = new EdibleEgg();
+        EdibleEgg egg3 = new EdibleEgg();
+        EdibleEgg egg4 = new EdibleEgg();
+        EdibleEgg egg5 = new EdibleEgg();
+        EdibleEgg egg6 = new EdibleEgg();
+        EdibleEgg egg7 = new EdibleEgg();
 
+        boolean inStable1 = stable1.addHorse(horse1);
+        boolean inStable2 = stable2.addHorse(horse2);
+        assertTrue(inStable1);
+        assertTrue(inStable2);
+
+        // WHEN
+        farmer.mount(horse1);
+        farmer2.mount(horse2);
+
+        farmer.feedHorse(horse1, corn1);
+        farmer.feedHorse(horse2, corn2);
+        farmer.feedHorse(horse1, corn3);
+        farmer.feedHorse(horse2, corn4);
+        farmer.feedHorse(horse1, corn5);
+        farmer.feedHorse(horse2, corn6);
+
+        farmer.eat(corn1);
+        farmer.eat(tomato1);
+        farmer.eat(tomato2);
+        farmer.eat(egg1);
+        farmer.eat(egg2);
+        farmer.eat(egg3);
+        farmer.eat(egg4);
+        farmer.eat(egg5);
+
+        farmer2.eat(corn2);
+        farmer2.eat(corn3);
+        farmer2.eat(tomato3);
+        farmer2.eat(egg6);
+        farmer2.eat(egg7);
+
+        // THEN
+        assertEquals(1, farmer.getCornCount());
+        assertEquals(2, farmer.getTomatoCount());
+        assertEquals(5, farmer.getEggCount());
+        assertEquals(2, farmer2.getCornCount());
+        assertEquals(1, farmer2.getTomatoCount());
+        assertEquals(2, farmer2.getEggCount());
     }
-
 }

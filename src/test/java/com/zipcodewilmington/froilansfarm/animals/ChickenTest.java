faf1a37a2f5ece715ee3c041450a65b38064d61b
Mohammed amine ztait <Mztait@wil.yearup.org>
2023-05-21 12:50:22 -0400

    ChickenTest

--- src/test/java/com/zipcodewilmington/froilansfarm/animals/ChickenTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/animals/ChickenTest.java
@@ -1,2 +1,33 @@
-package com.zipcodewilmington.froilansfarm.animals;public class ChickenTest {
+package com.zipcodewilmington.froilansfarm.animals;
+
+import com.zipcodewilmington.froilansfarm.EdibleEgg;
+import com.zipcodewilmington.froilansfarm.interfaces.Edible;
+import org.junit.jupiter.api.Test;
+
+public class ChickenTest {
+
+    @Test
+    void testMakeNoise() {
+        Chicken chicken = new Chicken();
+        chicken.makeNoise();
+    }
+
+    @Test
+    void testYield() {
+        Chicken chicken = new Chicken();
+        chicken.hasBeenFertilized = true;
+        Edible egg = chicken.yield();
+
+
+        chicken.hasBeenFertilized = false;
+        Edible noEgg = chicken.yield();
+
+    }
+
+    @Test
+    void testEat() {
+        Chicken chicken = new Chicken();
+
+        chicken.eat(new Edible() {});
+    }
 }
